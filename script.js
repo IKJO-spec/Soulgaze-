dccb12f599ff0212373885c0aa81a7e6d0434ee9
IKJO-spec <isaackasulejo16@gmail.com>
2025-03-24 09:00:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function downloadImage(imagePath) {
+    const link = document.createElement("a");
+    link.href = imagePath;
+    link.download = imagePath.split('/').pop(); // Extracts filename
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+
