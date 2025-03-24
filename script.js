fbbbdde00430893432759b5b954d23f36b596810
Xylo <isaackasulejo16@gmail.com>
2025-03-24 20:18:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function downloadImage(imagePath) {
     const link = document.createElement("a");
     link.href = imagePath;
-    link.download = imagePath.split('/').pop(); // Extracts filename
+    link.download = imagePath.split('').pop(); // Extracts filename
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
